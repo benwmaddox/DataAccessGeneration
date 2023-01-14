b144c06a0f331e04f0b2ada7c5d4c00827c91b3f
Ben Maddox <benwmaddox@gmail.com>
2023-01-14 17:13:30 +0000

    Working on tokens

--- DataAccessGeneration/ScriptParsing.cs
+++ DataAccessGeneration/ScriptParsing.cs
@@ -1,12 +1,14 @@
 using Microsoft.SqlServer.TransactSql.ScriptDom;
 
-public class ScriptParsing{
-    public object FindParametersInQuery(string query){
+public class ScriptParsing
+{
+    public string FindTokensInQuery(string query)
+    {
         // using scriptdom, find all parameters in the query
         var parser = new TSql120Parser(true);
         IList<ParseError> errors;
         var script = parser.Parse(new StringReader(query), out errors);
-        
-        return script;
+        var tokens = script.ScriptTokenStream;
+        return tokens;
     }
 }
